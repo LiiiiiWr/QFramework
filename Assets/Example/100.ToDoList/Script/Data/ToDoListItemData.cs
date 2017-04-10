5079576e78a2ea6fd4bb5ed1ab2e180baaf210a9
liangxieq <liangxiegame@gmail.com>
2017-04-10 22:13:21 +0800

    0.0.5.45 ToDoList存储部分使用ProtoBuf和Json完成

--- Assets/Example/100.ToDoList/Script/Data/ToDoListItemData.cs
+++ Assets/Example/100.ToDoList/Script/Data/ToDoListItemData.cs
@@ -3,29 +3,3 @@ using System.Collections.Generic;
 using UnityEngine;
 
 
-namespace ToDoList {
-	
-	public class ToDoListItemData {
-
-		public string Id;
-
-		public string Content;
-
-		bool m_Complete;
-		public bool Complete {
-			get {
-				return m_Complete;
-			}
-			set {
-				m_Complete = value;
-			}
-		}
-
-		public bool Deleted = false;
-
-		public void Description() {
-			Debug.Log (Id + ":" + Complete + ":" + Content + ":" + Deleted);
-		}
-	}
-
-}
