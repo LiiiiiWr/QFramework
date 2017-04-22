ed26302fcd85f25dbb646c3884443dbf4ac17258
liangxiegame <liangxiegame@163.com>
2017-04-23 00:45:17 +0800

    0.0.5.64 add license

--- Assets/Example/0.ProgrammingStyle/ProgrammingStyle.cs
+++ Assets/Example/0.ProgrammingStyle/ProgrammingStyle.cs
@@ -1,15 +1,40 @@
-﻿using UnityEngine;
+﻿/****************************************************************************
+ * Copyright (c) 2017 liangxie
+ * 
+ * http://liangxiegame.com
+ * https://github.com/liangxiegame/QFramework
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+****************************************************************************/
+
+using UnityEngine;
 using UnityEngine.UI;
 using System.Collections;
 using System.Collections.Generic;
 
-
-namespace QFramework.Example {
-	
+namespace QFramework.Example 
+{	
 	/// <summary>
 	/// 展示编码风格
 	/// </summary>
-	public class ProgrammingStyle : MonoBehaviour {
+	public class ProgrammingStyle : MonoBehaviour 
+	{
 		#region Basic App
 		/// <summary>
 		/// 1.private/protected使用m开头+驼峰式
@@ -25,13 +50,15 @@ namespace QFramework.Example {
 		/// <summary>
 		/// public 类型属性也算public类型变量
 		/// </summary>
-		public int CurSelectIndex {
+		public int CurSelectIndex 
+		{
 			get {
 				return mCurSelectIndex;
 			}
 		}
 
-		void Start () {
+		void Start () 
+		{
 			mBtnEnterMainPage = transform.Find ("BtnEnterMainPage").GetComponent<Button>();
 
 			// GameObject命名
@@ -42,7 +69,8 @@ namespace QFramework.Example {
 		/// <summary>
 		/// 方法名一律首字母大写驼峰式
 		/// </summary>
-		public void Hide() {
+		public void Hide() 
+		{
 			gameObject.SetActive (false);
 		}
 		#endregion
