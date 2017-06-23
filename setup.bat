a657b7a05e00394f9005fd64cd7339b3c1ac08dd
liangxieq <liangxiegame@gmail.com>
2017-06-23 11:26:25 +0800

    0.0.5.103 windows support

---
+++ setup.bat
@@ -0,0 +1,2 @@
+mklink /J ../../../../Submodule/QSingleton/Client/Assets/QSingleton ./Client/Assets/QFramework/Core
+mv ./Client/Assets/QFramework/Core/QSingleton ./Client/Assets/QFramework/Core/Singleton
--- setup.sh
+++ setup.sh
@@ -1,3 +1,5 @@
 # QSingleton
+rm ./Client/Assets/QFramework/Core/Singleton
 ln -s ../../../../Submodule/QSingleton/Client/Assets/QSingleton ./Client/Assets/QFramework/Core
 mv ./Client/Assets/QFramework/Core/QSingleton ./Client/Assets/QFramework/Core/Singleton
+echo "Setup finish !"
