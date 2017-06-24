a1972f8faaebf27b181e05e2a0269ce799a46228
liangxiegame <liangxiegame@163.com>
2017-06-24 17:18:48 +0800

    0.0.5.106 win use qframework

--- setup.bat
+++ setup.bat
@@ -1,1 +1,1 @@
-mklink /J .\Client\Assets\QFramework\Core\Singleton\Win  .\Submodule\QSingleton\Client\Assets\QSingleton
+mklink /J .\Client\Assets\QFramework\Core\QSingleton  .\Submodule\QSingleton\Client\Assets\QSingleton
